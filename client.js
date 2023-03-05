d37b8b43df236507319bbdc0d618b62bdc11a249
cch137 <96408836+cch137@users.noreply.github.com>
2023-03-06 04:40:05 +0800

    updated

--- client.js
+++ client.js
@@ -1,160 +1,200 @@
-const io = (() => {
-  const $CONNECT = 'connect';
-  const $DISCONNECT = 'disconnect';
-  const $ERROR = 'error';
-  const $RECONNECT = 'reconnect';
-  const $RECONNECTING = 'reconnecting';
-  const $RECONNECT_ATTEMPT = 'reconnect_attempt';
-  const $RECONNECT_ERROR = 'reconnect_error';
-  const $RECONNECT_FAILED = 'reconnect_failed';
+const $CONNECT = 'connect';
+const $DISCONNECT = 'disconnect';
+const $ERROR = 'error';
+const $RECONNECT = 'reconnect';
+const $RECONNECTING = 'reconnecting';
+const $RECONNECT_ATTEMPT = 'reconnect_attempt';
+const $RECONNECT_ERROR = 'reconnect_error';
+const $RECONNECT_FAILED = 'reconnect_failed';
 
-  class IO {
-    connected = false;
-    reconnecting = false;
-    reconnectTries = 0;
-    handlers = new Map();
+/**
+ * @typedef {Object} WSEventData
+ * @property {*} data
+ * @property {String} [event]
+ * @property {Number} [id]
+ */
 
-    get reserved_events() {
-      return [
-        $CONNECT,
-        $DISCONNECT,
-        $ERROR,
-        $RECONNECT,
-        $RECONNECTING,
-        $RECONNECT_ATTEMPT,
-        $RECONNECT_ERROR,
-        $RECONNECT_FAILED
-      ];
-    }
+/**
+ * @callback WSHandler
+ * @param {WSEventData} eventData
+ */
 
-    constructor() {
-      this.connect();
-    }
+class IO {
+  /** @type {Boolean} */
+  connected = false;
 
-    callListeners(eventName, eventData) {
-      if (this.handlers.has(eventName)) {
-        this.handlers.get(eventName).forEach(h => {
-          try {
-            h(eventData);
-          } catch (err) {
-            console.error(err);
-          }
-        });
-      }
-    }
-    
-    on(eventName, handler=(event)=>{}) {
-      if (!this.handlers.has(eventName)) {
-        this.handlers.set(eventName, new Set([handler]));
-      } else {
-        this.handlers.get(eventName).add(handler);
-      }
+  /** @type {Boolean|NodeJS.Timer|Number} */
+  reconnecting = false;
 
-      if (eventName === $CONNECT) {
-        if (this.connected) handler(this.connected);
-      }
-    }
+  /** @type {Boolean} */
+  autoReconnect = true;
 
-    delHandler(eventName, handler) {
-      if (this.handlers.has(eventName)) {
-        this.handlers.get(eventName).delete(handler);
-        if (!this.handlers.get(eventName).size) {
-          this.handlers.delete(eventName);
-        }
-      }
-    }
+  /** @type {Number} */
+  reconnectTries = 0;
 
-    clearHandlers(eventName) {
-      if (this.handlers.has(eventName)) {
-        this.handlers.delete(eventName);
-      }
-    }
-    
-    connect() {
-      const self = this;
+  /** @type {Map<String|Number, Function>} */
+  handlers = new Map();
 
-      if (self.reconnectTries > 1) self.callListeners($RECONNECT_FAILED);
-      if (self.reconnecting) self.callListeners($RECONNECT_ATTEMPT);
-      const ws = new WebSocket(location.origin.replace(/^https?:\/\//, 'ws://'));
+  get reserved_events() {
+    return [
+      $CONNECT,
+      $DISCONNECT,
+      $ERROR,
+      $RECONNECT,
+      $RECONNECTING,
+      $RECONNECT_ATTEMPT,
+      $RECONNECT_ERROR,
+      $RECONNECT_FAILED
+    ];
+  }
 
-      ws.onopen = (event) => {
-        self.connected = event || true;
-        if (self.reconnecting) {
-          self.callListeners($RECONNECT, event);
-          clearInterval(self.reconnecting);
-          self.reconnecting = false;
-          self.reconnectTries = 0;
-        } else {
-          self.callListeners($CONNECT, event);
-        }
-      };
+  /** @param {Boolean} connectNow */
+  constructor(connectNow=true) {
+    if (connectNow) this.connect();
+  }
 
-      ws.onmessage = (event) => {
-        let eventName = 'message', data = event.data;
+  /** @param {String|Number} eventName @param {WSEventData} eventData */
+  callListeners(eventName, eventData) {
+    if (this.handlers.has(eventName)) {
+      this.handlers.get(eventName).forEach(h => {
         try {
-          const packet = JSON.parse(data);
-          if (packet.event === 'pending') {
-            eventName = packet.data.id;
-            data = packet.data.data;
-          } else {
-            eventName = packet.event;
-            data = packet.data;
-          }
-        } catch {}
-        self.callListeners(eventName, data);
-      };
-
-      ws.onerror = (err) => {
-        if (self.reconnecting) {
-          self.callListeners($RECONNECT_ERROR, err);
-        } else {
-          self.callListeners($ERROR, err);
+          h(eventData);
+        } catch (err) {
+          console.error(err);
         }
-      };
-
-      ws.onclose = (event) => {
-        self.connected = false;
-        if (!self.reconnecting) {
-          self.reconnecting = setInterval(() => {
-            self.callListeners($RECONNECTING);
-          }, 0);
-        }
-        self.reconnectTries++;
-        self.connect();
-        self.callListeners($DISCONNECT, event);
-      };
-      
-      delete this.ws;
-      this.ws = ws;
-    }
-
-    emit(event, data) {
-      this.ws.send(JSON.stringify({
-        event, data
-      }));
-    }
-
-    #pendingCount = 0;
-
-    pending(event, data) {
-      return new Promise((resolve, reject) => {
-        const id = ++this.#pendingCount;
-        this.ws.send(JSON.stringify({
-          event: 'pending',
-          data: {
-            id, event, data
-          }
-        }));
-        this.on(id, (data, err) => {
-          if (err) reject(err);
-          else resolve(data);
-          this.clearHandlers(id)
-        });
       });
     }
   }
+  
+  /** @param {String|Number} eventName @param {WSHandler} handler */
+  on(eventName, handler) {
+    if (!this.handlers.has(eventName)) {
+      this.handlers.set(eventName, new Set([handler]));
+    } else {
+      this.handlers.get(eventName).add(handler);
+    }
+    if (eventName === $CONNECT) {
+      if (this.connected) handler();
+    }
+  }
 
-  return () => new IO();
-})();
+  /** @param {String|Number} eventName @param {WSHandler} handler */
+  delHandler(eventName, handler) {
+    if (this.handlers.has(eventName)) {
+      this.handlers.get(eventName).delete(handler);
+      if (!this.handlers.get(eventName).size) {
+        this.handlers.delete(eventName);
+      }
+    }
+  }
+
+  /** @param {String|Number} eventName */
+  clearHandlers(eventName) {
+    if (this.handlers.has(eventName)) {
+      this.handlers.delete(eventName);
+    }
+  }
+  
+  connect() {
+    const self = this;
+
+    self.autoReconnect = true;
+    if (self.reconnectTries > 1) self.callListeners($RECONNECT_FAILED);
+    if (self.reconnecting) self.callListeners($RECONNECT_ATTEMPT);
+
+    const ws = new WebSocket(location.origin.replace(/^https?:\/\//, 'ws://'));
+
+    ws.onopen = (event) => {
+      self.connected = true;
+      if (self.reconnecting) {
+        self.callListeners($RECONNECT, event);
+        clearInterval(self.reconnecting);
+        self.reconnecting = false;
+        self.reconnectTries = 0;
+      } else {
+        self.callListeners($CONNECT, event);
+      }
+    };
+
+    /** @param {WSEventData} event */
+    ws.onmessage = (event) => {
+      let eventName = 'message';
+      /** @type {WSEventData} */
+      let data = event.data;
+      try {
+        /** @type {WSEventData} */
+        const packet = JSON.parse(data);
+        if (packet.event === 'pending') {
+          eventName = packet.data.id;
+          data = packet.data.data;
+        } else {
+          eventName = packet.event;
+          data = packet.data;
+        }
+      } catch {}
+      self.callListeners(eventName, data);
+    };
+
+    ws.onerror = (err) => {
+      if (self.reconnecting) {
+        self.callListeners($RECONNECT_ERROR, err);
+      } else {
+        self.callListeners($ERROR, err);
+      }
+    };
+
+    ws.onclose = (event) => {
+      self.connected = false;
+      if (!self.reconnecting) {
+        self.reconnecting = setInterval(() => {
+          self.callListeners($RECONNECTING);
+        }, 0);
+      }
+      self.reconnectTries++;
+      if (self.autoReconnect) self.connect();
+      self.callListeners($DISCONNECT, event);
+    };
+    
+    delete this.ws;
+    this.ws = ws;
+  }
+
+  disconnect() {
+    this.autoReconnect = false;
+    this.ws.close(1000, 'Connection closed by user');
+  }
+
+  
+  /** @param {String} event @param {*} data */
+  emit(event, data) {
+    this.ws.send(JSON.stringify({
+      event, data
+    }));
+  }
+
+  /** @type {Number} */
+  #pendingCount = 0;
+
+  /** @param {String} event @param {*} data */
+  pending(event, data) {
+    return new Promise((resolve, reject) => {
+      const id = ++this.#pendingCount;
+      this.ws.send(JSON.stringify({
+        event: 'pending',
+        data: {
+          id, event, data
+        }
+      }));
+      this.on(id, (data, err) => {
+        if (err) reject(err);
+        else resolve(data);
+        this.clearHandlers(id)
+      });
+    });
+  }
+}
+
+const io = () => new IO();
+io.IO = IO;
 
 export default io;
